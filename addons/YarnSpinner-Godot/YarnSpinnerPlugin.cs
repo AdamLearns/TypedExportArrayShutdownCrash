6b825741fda3626a702c34e0cf446e92b5670a35
Adam Damiano <adam@adamlearns.com>
2026-06-04 23:06:24 -0700

    Remove extraneous code

--- addons/YarnSpinner-Godot/YarnSpinnerPlugin.cs
+++ addons/YarnSpinner-Godot/YarnSpinnerPlugin.cs
@@ -5,30 +5,7 @@ namespace YarnSpinnerGodot;
 [Tool]
 public partial class YarnSpinnerPlugin : EditorPlugin
 {
-    public override void _EnterTree()
-    {
-        RegisterCustomType<DialogueRunner>("DialogueRunner", "Node", "res://addons/YarnSpinner-Godot/Runtime/DialogueRunner.cs");
-        RegisterCustomType<LinePresenter>("LinePresenter", "Node", "res://addons/YarnSpinner-Godot/Runtime/Async/LinePresenter.cs");
-        RegisterCustomType<OptionsPresenter>("OptionsPresenter", "Node", "res://addons/YarnSpinner-Godot/Runtime/Async/OptionsPresenter.cs");
-        RegisterCustomType<BasicMarker>("BasicMarker", "Resource", "res://addons/YarnSpinner-Godot/Runtime/BasicMarker.cs");
-        RegisterCustomType<CustomMarker>("CustomMarker", "Resource", "res://addons/YarnSpinner-Godot/Runtime/CustomMarker.cs");
-        RegisterCustomType<MarkupPalette>("MarkupPalette", "Resource", "res://addons/YarnSpinner-Godot/Runtime/Views/MarkupPalette.cs");
-    }
+    public override void _EnterTree() { }
 
-    public override void _ExitTree()
-    {
-        RemoveCustomType(nameof(DialogueRunner));
-        RemoveCustomType(nameof(LinePresenter));
-        RemoveCustomType(nameof(OptionsPresenter));
-        RemoveCustomType(nameof(BasicMarker));
-        RemoveCustomType(nameof(CustomMarker));
-        RemoveCustomType(nameof(MarkupPalette));
-    }
-
-    private void RegisterCustomType<T>(string typeName, string baseType, string scriptPath)
-        where T : GodotObject
-    {
-        var script = ResourceLoader.Load<CSharpScript>(scriptPath);
-        AddCustomType(typeName, baseType, script, null);
-    }
+    public override void _ExitTree() { }
 }
